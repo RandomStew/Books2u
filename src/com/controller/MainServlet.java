0dd8236539c543ca176d4c238abed8bcde7674cf
HyeonGyu <marrin1101@gmail.com>
2022-02-25 03:47:23 +0900

    add

--- src/com/controller/MainServlet.java
+++ src/com/controller/MainServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/MainServlet")
@@ -13,7 +14,7 @@ public class MainServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		HttpSession session = request.getSession();
 		request.getRequestDispatcher("main.jsp").forward(request, response);
 	}
 
